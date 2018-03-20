53c33125c172c0a13a7f1fee335205a9876a344e
José A. Barbero <jo94se@hotmail.com>
2018-03-20 19:51:19 +0100

    Exception management and chest functionality.

--- lolAssistant.py
+++ lolAssistant.py
@@ -4,6 +4,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import discord
 import os
+from requests import HTTPError
 
 prefix="!"
 client = Bot(description="Fate bot for discord.", command_prefix=prefix, pm_help = False)
@@ -27,8 +28,11 @@ def getCurrentGamePlayers(game):
 
 def getCurrentGameData(summonerName):
     ret_str = []
-    summonerId = watcher.summoner.by_name(my_region, summonerName)["id"]
-    game = watcher.spectator.by_summoner(my_region, summonerId)
+    try:
+        summonerId = watcher.summoner.by_name(my_region, summonerName)["id"]
+        game = watcher.spectator.by_summoner(my_region, summonerId)
+    except HTTPError as err:
+        return 'Probably this summoner is not currently in a game or his name is wrong.'
     sides = getCurrentGamePlayers(game)
     sideNames = ["Blue", "Red"]
     for i in range(len(["Blue", "Red"])):
@@ -52,10 +56,27 @@ def getCurrentGameData(summonerName):
                 line.append("W/L: NODATA")
                 line.append("\t|\t")
                 line.append("NODATA")
+            line.append("\t|\t")
+            line.append(p["summonerName"])
             line.append("\n")
             ret_str.append(''.join(line))
         ret_str.append("\n\n")
     return "".join(ret_str)
+
+def gotChest(my_region, sum_name, champ_name):
+    try:
+        sum_id = watcher.summoner.by_name(my_region, sum_name)["id"]
+    except HTTPError as err:
+        print('Wrong summoner name.')
+        return None
+    try:
+        champ_id = df_champs.loc[df_champs.name.str.lower() == champ_name.lower(), "id"].values[0]
+    except:
+        print('Wrong champion name.')
+        return None
+    sum_id = watcher.summoner.by_name(my_region, sum_name)["id"]
+    champ_id = df_champs.loc[df_champs.name.str.lower() == champ_name.lower(), "id"].values[0]
+    return watcher.champion_mastery.by_summoner_by_champion(my_region, sum_id, champ_id)["chestGranted"]
 
 
 @client.event
@@ -68,7 +89,7 @@ async def on_ready():
 
 @client.command(pass_context=True)
 async def game(ctx, summoner="empty"):
-    """Sends a predefined number of dices."""
+    """Sends the current game info."""
     if summoner == "empty":
         await client.say("Plase, enter a correct summoner name.")
     else:
@@ -76,9 +97,24 @@ async def game(ctx, summoner="empty"):
         await client.say(getCurrentGameData(summoner))
 
 @client.command(pass_context=True)
+async def canchest(ctx, summoner="empty", champion="empty"):
+    """Tells if tha given summoner can get a chest with the given champion."""
+    if summoner == "empty" or champion=="empty":
+        await client.say("Plase, enter a correct summoner name and a correct champion.")
+    else:
+        res = gotChest(my_region, summoner, champion)
+        if res is not None:
+            if res:
+                await client.say(f"No, sorry. You already got an S with {champion}.")
+            else:
+                await client.say(f"Yes, you don't have a chest with {champion} yet.")
+        else:
+            await client.say("Plase, enter a correct summoner name and a correct champion.")
+                        
+
+@client.command(pass_context=True)
 async def clear(ctx):
     """ Clears every bot message from the channel. """
     await client.purge_from(ctx.message.channel, limit=200, check=lambda m: (m.author == client.user) or m.content.startswith(prefix))
 
-#def set_picture(url):
 client.run(os.environ['DISCORD_KEY'])
